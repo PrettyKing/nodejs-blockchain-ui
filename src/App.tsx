cca5334d4837ab88cc2e24aa78cb6dea1bd7b177
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-05-20 10:04:24 +0800

    添加src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import Blockchain from './pages/Blockchain'
+import Wallet from './pages/Wallet'
+import Transaction from './pages/Transaction'
+import Mining from './pages/Mining'
+import { useState } from 'react'
+
+function App() {
+  const [isConnected, setIsConnected] = useState(false)
+  
+  return (
+    <div className="min-h-screen">
+      <Routes>
+        <Route path="/" element={<Layout isConnected={isConnected} setIsConnected={setIsConnected} />}>
+          <Route index element={<Dashboard isConnected={isConnected} />} />
+          <Route path="blockchain" element={<Blockchain />} />
+          <Route path="wallet" element={<Wallet />} />
+          <Route path="transaction" element={<Transaction />} />
+          <Route path="mining" element={<Mining />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
